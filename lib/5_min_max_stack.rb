38498db024ecfc4c7b77b65a740f47dee991c112
Spencer <spenjlee@gmail.com>
2019-06-26 17:18:22 -0700

    Implement MinMaxStack to pass all tests

---
+++ lib/5_min_max_stack.rb
@@ -0,0 +1,40 @@
+require_relative '3_my_stack'
+
+class MinMaxStack < MyStack
+	def initialize
+		@store = []
+		@maxes = []
+		@mins  = []
+	end
+
+	def push(value)
+		@store << value
+
+		return set_max_and_min(value) if @maxes.empty? && @mins.empty?
+
+		@maxes << ( value > max ? value : max )
+		@mins  << ( value < min ? value : min )
+	end
+
+	def pop
+		[@store, @maxes, @mins].each(&:pop)
+	end
+
+	def max
+		@maxes.last
+	end
+
+	def min
+		@mins.last
+	end
+
+	def empty?
+		@store.empty?
+	end
+
+	private
+	def set_max_and_min(value)
+		@maxes << value
+		@mins  << value
+	end
+end

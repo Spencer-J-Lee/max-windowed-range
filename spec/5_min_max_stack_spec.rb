ee1f5d6bf527b0c93a9c086fe8802552318c1cf7
Spencer <spenjlee@gmail.com>
2019-06-26 17:17:21 -0700

    Adjust tests to use numbers for proper testing

--- spec/5_min_max_stack_spec.rb
+++ spec/5_min_max_stack_spec.rb
@@ -5,77 +5,75 @@ describe MinMaxStack do
 	subject(:minmaxstack) { MinMaxStack.new }
 
 	describe "#initialize" do
-		it "instantiates an empty array held in @store" do
+		it "instantiates empty arrays held in @store, @maxes, and @mins" do
 			expect(minmaxstack.instance_variable_get(:@store)).to eq([])
+			expect(minmaxstack.instance_variable_get(:@maxes)).to eq([])
+			expect(minmaxstack.instance_variable_get(:@mins)).to eq([])
 		end
 	end
 
 	describe "#push" do
 		it "adds an element onto the top of the stack" do
-			minmaxstack.push("bottom")
-			minmaxstack.push("top")
-			expect(minmaxstack.instance_variable_get(:@store).last).to eq("top")
+			minmaxstack.push(1)
+			minmaxstack.push(2)
+			expect(minmaxstack.instance_variable_get(:@store).last).to eq(2)
 		end
+	end
+
+	let(:minmaxstack_123) do
+		minmaxstack.push(1)
+		minmaxstack.push(2)
+		minmaxstack.push(3)
+		minmaxstack
 	end
 
 	describe "#pop" do
 		it "removes an element from the top of the stack" do
-			minmaxstack.push("bottom")
-			minmaxstack.push("top")
-			minmaxstack.pop
-			expect(minmaxstack.instance_variable_get(:@store).last).to eq("bottom")
+			minmaxstack_123.pop
+			expect(minmaxstack_123.instance_variable_get(:@store).last).to eq(2)
 		end
 	end
 
 	describe "#max" do
 		it "returns the maximum value in the stack" do
-			minmaxstack.push(1)
-			minmaxstack.push(2)
-			minmaxstack.push(3)
-			expect(minmaxstack.max).to eq(3)
+			expect(minmaxstack_123.max).to eq(3)
 		end
 	end
 	
 	describe "#min" do
 		it "returns the minimum value in the stack" do
-			minmaxstack.push(1)
-			minmaxstack.push(2)
-			minmaxstack.push(3)
-			expect(minmaxstack.min).to eq(1)
+			expect(minmaxstack_123.min).to eq(1)
 		end
 	end
 
 	describe "#peek" do
-		before(:each) do
-			minmaxstack.push("bottom")
-			minmaxstack.push("top")
-		end
-
 		it "returns the element on top of the stack" do
-			expect(minmaxstack.peek).to eq("top")
+			expect(minmaxstack_123.peek).to eq(3)
 		end
 
 		it "doesn't remove the element" do
-			minmaxstack.peek
-			expect(minmaxstack.instance_variable_get(:@store)).to include("top")
+			minmaxstack_123.peek
+			expect(minmaxstack_123.instance_variable_get(:@store)).to include(2)
 		end
 	end
 
 	describe "#size" do
 		it "returns the size of the stack" do
-			expect(minmaxstack.size).to eq(0)
-			minmaxstack.push(1)
-			minmaxstack.push(2)
-			minmaxstack.push(3)
-			expect(minmaxstack.size).to eq(3)
+			expect(minmaxstack_123.size).to eq(3)
+			minmaxstack_123.pop
+			minmaxstack_123.pop
+			minmaxstack_123.pop
+			expect(minmaxstack_123.size).to eq(0)
 		end
 	end
 
 	describe "#empty?" do
 		it "determines if the stack is empty or not" do
-			expect(minmaxstack.empty?).to be(true)
-			minmaxstack.push("something")
-			expect(minmaxstack.empty?).to be(false)
+			expect(minmaxstack_123.empty?).to be(false)
+			minmaxstack_123.pop
+			minmaxstack_123.pop
+			minmaxstack_123.pop
+			expect(minmaxstack_123.empty?).to be(true)
 		end
 	end
 end
